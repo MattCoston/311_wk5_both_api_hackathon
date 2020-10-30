c91a0810234bc836ebc8917d7f2f7d36613e073b
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-29 20:38:22 -0500

    needed to rename the paths

--- routes/employee.js
+++ routes/employee.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.router();
+
+// Grabbing the Chris's controller folder
 const employeesController = require("../controllers/employees");
-router.get("/employees");
+//  Get all employees
+router.get("/employees", employeesController.getEmployees);
+//  Get employees by ID
+router.get("/employees/:id", employeesController.getEmployeesById);
 module.exports = router;
