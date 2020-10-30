dc92b396b2cc4d23590b19005922eaca5a50dde9
Chris Trevino <chrisdadef@gmail.com>
2020-10-30 18:41:07 -0500

    test commit

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,6 +1,5 @@
-
-const getEmployees = (req,res,next) {
-res.send("getting employees")
+const getEmployees = (req,res,next)  {
+ res.send("getting employees")
 
 }
 const getEmployeesById = (req,res,next) {
@@ -10,7 +9,7 @@ const getEmployeesById = (req,res,next) {
 
 const getEmployeesByFirstName = (req,res,next) {
   res.send("getting employees")  
-  
+
 }
 
 
