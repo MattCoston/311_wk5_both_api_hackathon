e6f67631cbfbebbe5e89da77fc1bab2bc6967e8d
Chris Trevino <chrisdadef@gmail.com>
2020-10-30 17:24:53 -0500

    updated res,send

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,12 +1,15 @@
 
 const getEmployees = (req,res,next) {
+res.send("getting employees")
 
 }
 const getEmployeesById = (req,res,next) {
-  
+  res.send("getting employees")
+
 }
 
 const getEmployeesByFirstName = (req,res,next) {
+  res.send("getting employees")  
   
 }
 
