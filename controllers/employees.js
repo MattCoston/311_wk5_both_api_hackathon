8a44b25b84de7c20ec46f475fef105766d25b355
Chris Trevino <chrisdadef@gmail.com>
2020-10-31 08:19:49 -0500

    test

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,13 +1,20 @@
+const mysql = require('../mysql/connection')
+
+
+
 const getEmployees = (req,res,next) => {
+
  res.send("getting employees")
 
 }
 const getEmployeesById = (req,res,next) => {
+
   res.send("getting employees")
 
 }
 
 const getEmployeesByFirstName = (req,res,next) => {
+  
   res.send("getting employees")  
 
 }
