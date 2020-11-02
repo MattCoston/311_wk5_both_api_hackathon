de98054ec7423f6e1bc530c5d60aa3c76a5ef7e8
AustinSkrobar <65634947+AustinSkrobar@users.noreply.github.com>
2020-11-02 02:17:54 -0600

    added api function to index page. As well as created error handing folder and files for that purpose.

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,13 +1,28 @@
+const apiError = require("../error/ApiError");
 
 const getEmployees = (req,res,next) {
-
+    if (!employees) {
+        next(apiError.badRequest("msg field is required and must be non blank"));
+        return;
+      }
+      //put code here
 }
 const getEmployeesById = (req,res,next) {
-  
+    //create variables here
+
+    if (!employee) {
+        next(apiError.badRequest("No such user exist."));
+        return;
+      }
+      //put response here
 }
 
 const getEmployeesByFirstName = (req,res,next) {
   
+    if (!name) {
+        next(apiError.badRequest("No such user exist."));
+        return;
+      }
 }
 
 
