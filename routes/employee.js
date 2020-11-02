dc9e64ebd731d55f2186f9db425425cee56b1ce2
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-11-02 15:53:22 -0600

    added first name to router

--- routes/employee.js
+++ routes/employee.js
@@ -7,4 +7,9 @@ const employeesController = require("../controllers/employees");
 router.get("/employees", employeesController.getEmployees);
 //  Get employees by ID
 router.get("/employees/:id", employeesController.getEmployeesById);
+//  Get employees by first name
+router.get(
+  "/employees/firstname/:first_name",
+  employeesController.getEmployeesByFirstName
+);
 module.exports = router;
