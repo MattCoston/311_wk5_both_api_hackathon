baa43c1e1919f974b863326e051e8b0e1d197cdf
Chris Trevino <chrisdadef@gmail.com>
2020-11-02 18:09:40 -0600

    finished

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,23 +1,43 @@
-const mysql = require('../mysql/connection')
+const mysql = require('mysql')
+const pool = require('../mysql/connection')
+const { apiErrorHandler } = require('../mysql/')
 
 
 
-const getEmployees = (req,res,next) => {
 
- res.send("getting employees")
+const getEmployees = (req,res) => {
+
+  //select no more than 50 from the employees list 
+  pool.query("SELECT * FROM employees LIMIT 50", (err, rows) => {
+    if(err) return apiErrorHandler(res, err)
+    return res.json(rows)
+  })
 
 }
-const getEmployeesById = (req,res,next) => {
-
-  res.send("getting employees")
-
+const getEmployeesById = (req,res) => {
+ 
+ 
+  //return individual who employee numbers match the number id provided 
+  let sql = "SELECT * FROM employees WHERE emp_no = ?"
+  sql = mysql.format(sql, [req.params.id])
+    pool.query(sql, (err, rows) => {
+    if(err) return apiErrorHandler(res, err)
+    return res.json(rows)
+})
 }
 
-const getEmployeesByFirstName = (req,res,next) => {
+const getEmployeesByFirstName = (req,res) => {
   
-  res.send("getting employees")  
+  let sql = "SELECT * FROM employees WHERE first_name = ?"
+  sql = mysql.format(sql, [req.params.first_name])
+  
+    pool.query(sql, (err, rows) => {
+    if(err) return apiErrorHandler(res, err)
+    return res.json(rows)
+  })}
+  
 
-}
+
 
 
 module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
