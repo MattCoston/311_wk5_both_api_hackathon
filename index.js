5730c1484f540e1fe9c5af8965332280e271bef5
Matt Coston <mattcoston7@gmail.com>
2020-11-03 19:41:42 -0600

    changed routes file as well added use in index.js main file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const apiErrorHandler = require("./error/api-error-handler");
+const employeesRouter = require("./routes/employee")
 
 const port = process.env.PORT || 4001;
 
 app.get("/", (req, res) => {
   res.send("Welcome to our API!");
 });
-
+app.use('/employees', employeesRouter);
 app.use(apiErrorHandler);
 
 app.listen(port, () => {
