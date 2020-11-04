c4a2e6032c4ecd286b78313415c6ea6ef0ac7cae
Matt Coston <mattcoston7@gmail.com>
2020-11-03 20:11:41 -0600

    added salaries controller file

---
+++ controllers/salaries.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+const pool = require("../mysql/connection");
+const { apiErrorHandler } = require('../error/ApiError');
+
+const getTopPaid = (req,res) => {
+
+}
+
+const getByRange = (req, res) => {
+    
+}
