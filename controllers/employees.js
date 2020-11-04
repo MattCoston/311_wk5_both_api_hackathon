2eb1bbeacc02ea61a02a4f83e1612c5fcc4bee5e
Jon Senterfitt <thetwosents@gmail.com>
2020-11-03 20:25:50 -0600

    Update employees.js

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-const mysql = require('mysql')
-const pool = require('../mysql/connection')
-const { apiErrorHandler } = require('../mysql/')
-
-
-
-
-const getEmployees = (req,res) => {
-
-  //select no more than 50 from the employees list 
-  pool.query("SELECT * FROM employees LIMIT 50", (err, rows) => {
-    if(err) return apiErrorHandler(res, err)
-    return res.json(rows)
-  })
-
-}
-const getEmployeesById = (req,res) => {
- 
- 
-  //return individual who employee numbers match the number id provided 
-  let sql = "SELECT * FROM employees WHERE emp_no = ?"
-  sql = mysql.format(sql, [req.params.id])
-    pool.query(sql, (err, rows) => {
-    if(err) return apiErrorHandler(res, err)
-    return res.json(rows)
-})
-}
-
-const getEmployeesByFirstName = (req,res) => {
-  
-  let sql = "SELECT * FROM employees WHERE first_name = ?"
-  sql = mysql.format(sql, [req.params.first_name])
-  
-    pool.query(sql, (err, rows) => {
-    if(err) return apiErrorHandler(res, err)
-    return res.json(rows)
-  })}
-  
-
-
-=======
 const mysql = require('mysql');
 const pool = require("../mysql/connection");
 const { apiErrorHandler } = require('../error/ApiError');
@@ -76,7 +34,6 @@ const getEmployeesByFirstName = (req,res) => {
         return res.json(results);
     })
 }
->>>>>>> 8ab3a8419872498e7c42f7d5ca6327526b548709
 
 
 module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
